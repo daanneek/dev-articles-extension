05cee890f653b2e7d960325f48d8def38c0f443c
daanneek <daanneek@gmail.com>
2023-10-07 12:16:27 +0200

    .

--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # SDA - Show data attributes
 
 ### A simple chrome extension to show data attributes. 
+
+
+# TODO
+
+### Figure out how to run scripts in browser DOM
+### Rewrite test.js (in TS)
+### Add branding
+### Expand Readme
--- src/popup/app.tsx
+++ src/popup/app.tsx
@@ -1,19 +1,12 @@
-import { useState } from 'preact/hooks'
 import './../app.css'
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Show data attributes</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <button onClick={() => chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) { 
             chrome.scripting.executeScript({target: {tabId: tabs[0].id ? tabs[0].id : 0}, files: ['./../content-scripts/test.js'] })
          })}>Do something</button>
-        <button onClick={() => setCount(0)}>reset</button>
       </div>
     </>
   )
